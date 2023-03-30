33e723fae55820d3f9635944eda8cff3caba64e4
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-03-30 12:41:42 +0500

    Initial commit

---
+++ src/main/java/springFrameworkSpringBoot/Spring6AuthServer/SpringFrameworkAndSpringBootAuthServerApplication.java
@@ -0,0 +1,13 @@
+package springFrameworkSpringBoot.Spring6AuthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkAndSpringBootAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkAndSpringBootAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springFrameworkSpringBoot/Spring6AuthServer/SpringFrameworkAndSpringBootAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package springFrameworkSpringBoot.Spring6AuthServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkAndSpringBootAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
